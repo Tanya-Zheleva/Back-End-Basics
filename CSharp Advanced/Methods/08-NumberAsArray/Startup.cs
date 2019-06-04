25d859be920b9f1407672f964d4a73b3beaf0a53
Tanya-Zheleva <tanya.zhelevaa@gmail.com>
2019-06-04 14:43:53 +0300

    Adding more tasks

---
+++ CSharp Advanced/Methods/08-NumberAsArray/Startup.cs
@@ -0,0 +1,28 @@
+﻿namespace _08_NumberAsArray
+{
+    using System;
+    using System.Linq;
+
+    public class Startup
+    {
+        public static void Main(string[] args)
+        {
+            int[] lengths = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            int n = lengths[0];
+            int m = lengths[1];
+
+            int[] firstNumber = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            int[] secondNumber = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+            int minLen = n >= m ? m : n;
+            int maxLen = n >= m ? n : m;
+            int currentDigitSum = 0;
+            int toAdd = 0;
+
+            for (int i = 0; i < minLen; i++)
+            {
+                currentDigitSum = firstNumber[i] + secondNumber[i];
+            }
+        }
+    }
+}
